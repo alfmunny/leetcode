6c062d276a5ae0d33378fa23b0997bf71bdcbf30
alfmunny <alfmunny@gmail.com>
2016-11-21 17:59:03 +0100

    add arithmetic slices

---
+++ 413 Arithmetic Slices/arithmetic_slices.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        if(A.size() < 3) return 0;
+        int count = 0, ret = 0;
+        for(int i = 1; i < A.size() - 1; i++)
+        {
+            if(A[i] - A[i-1] == A[i+1] - A[i]) ret += ++count; 
+            else count = 0;
+        }
+        return ret;
+    }
+};
