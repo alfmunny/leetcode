fe7d49d85d7529b5107ce8d3c90e15269431bf7d
alfmunny <alfmunny@gmail.com>
2020-02-19 16:55:23 +0100

    update

--- 41. First Missing Positive/solution.py
+++ 41. First Missing Positive/solution.py
@@ -1,6 +1,18 @@
 class Solution(object):
     def firstMissingPositive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        l = len(nums)
+        for i in range(l):
+            # note there has to be while
+            while (nums[i] > 0 and nums[i] <= l and nums[nums[i] - 1] != nums[i]):
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+
+        for i, n in enumerate(nums):
+            if (n != i+1):
+                return i + 1
+
+        return l + 1
+
+nums = [3, 4, -1, 1]
+nums = [3, 4, -1, 1]
+
+print(Solution().firstMissingPositive(nums))
