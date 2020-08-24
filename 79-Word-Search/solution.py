5a59edfc756253b757e9556d5a15bc1d3d7b7db1
alfmunny <alfmunny@gmail.com>
2020-08-24 18:34:06 +0200

    79 Word Search

--- 79-Word-Search/solution.py
+++ 79-Word-Search/solution.py
@@ -1,31 +1,26 @@
 class Solution:
-    def exist(self, board, word):
-        m = [[0 for j in range(len(board[0]))] for i in range(len(board))]
+    def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if self.exist_rec(board, word, i, j, m):
+                if self.dfs(board, word, 0, i, j):
                     return True
         return False
 
-    def exist_rec(self, board, word, i, j, m):
-        if len(word) == 0:
+    def dfs(self, board, word, w, i, j):
+        if w == len(word):
             return True
 
         if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]):
             return False
 
-        if board[i][j] == word[0] and m[i][j] == 0:
-            m[i][j] = 1
+        if board[i][j] == word[w]:
+            board[i][j] = '-'
 
-            if self.exist_rec(board, word[1:], i - 1, j, m) or \
-            self.exist_rec(board, word[1:], i + 1, j, m) or \
-            self.exist_rec(board, word[1:], i, j - 1, m) or \
-            self.exist_rec(board, word[1:], i, j + 1, m):
-                return True
-            else:
-                m[i][j] = 0
+            for next_i, next_j in [[i + 1, j], [i - 1, j], [i, j - 1],
+                                   [i, j + 1]]:
+                if self.dfs(board, word, w + 1, next_i, next_j):
+                    return True
+
+            board[i][j] = word[w]
 
         return False
-
-board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
-word = "ABCCED"
