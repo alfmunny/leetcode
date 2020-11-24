b2c5ed3c253fb5af1631703c609ed8a66d6731c7
alfmunny <alfmunny@gmail.com>
2020-11-24 21:39:19 +0100

    130 Surrounded Regions

--- 130-Surrounded-Regions/solution.py
+++ 130-Surrounded-Regions/solution.py
@@ -5,34 +5,29 @@ class Solution:
         """
         if not board or not board[0]:
             return
-
+        
         marked = [[False] * len(board[0]) for _ in board]
 
+        for i in [0, len(board)-1]:
+            for j in range(len(board[0])):
+                if board[i][j] == 'O':
+                    self.color(board, i, j, 'O', 'S')
+                
+        for i in range(1, len(board)-1):
+            for j in [0, len(board[0])-1]:
+                if board[i][j] == 'O':
+                    self.color(board, i, j, 'O', 'S')
+                
         for i in range(len(board)):
             for j in range(len(board[0])):
-                self.flag = False
-                if board[i][j] == 'O' and not marked[i][j]:
-                    self.dfs(i, j, board, marked)
+                board[i][j] = 'OX'[board[i][j] !='S']
 
-                    if not self.flag:
-                        self.color(i, j, board)
 
-    def dfs(self, i, j, board, marked):
-        marked[i][j] = True
-
-        for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-            if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]):
-                self.flag = True
-                continue
-
-            if marked[x][y]:
-                continue
-
-            if board[x][y] == 'O':
-                self.dfs(x, y, board, marked)
-
-    def color(self, i, j, board):
-        board[i][j] = 'X'
-        for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-            if board[x][y] == 'O':
-                self.color(x, y, board)
+    def color(self, board, i, j, target, symbol):
+        if i >= len(board) or j >= len(board[0]) or i < 0 or j < 0:
+            return
+        
+        if board[i][j] == target:
+            board[i][j] = symbol
+            for v, w in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                self.color(board, i+v, j+w, target, symbol)
